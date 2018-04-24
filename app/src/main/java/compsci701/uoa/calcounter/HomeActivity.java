bcf2d22c35be6acfe644fbb59f609cb5af268c86
nathan gavin <nathangavin987@gmail.com>
2018-04-24 13:31:29 +1200

    added user class, filled welcomeActivity.java

--- app/src/main/java/compsci701/uoa/calcounter/HomeActivity.java
+++ app/src/main/java/compsci701/uoa/calcounter/HomeActivity.java
@@ -2,26 +2,24 @@ package compsci701.uoa.calcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    private EditText heightText;
-    private TextView bmiView;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        heightText = (EditText) findViewById(R.id.height);
-        bmiView = (TextView) findViewById(R.id.bmi);
+
     }
 
-    private void calculate() {
-        Float height = Float.valueOf(heightText.getText().toString());
-
-        bmiView.setText("");
-    }
 }
--- app/src/main/java/compsci701/uoa/calcounter/WelcomeActivity.java
+++ app/src/main/java/compsci701/uoa/calcounter/WelcomeActivity.java
@@ -2,12 +2,77 @@ package compsci701.uoa.calcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.content.Intent;
+
+import java.util.ArrayList;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+
+    private EditText _nameText;
+    private EditText _ageText;
+    private Spinner _genderSpinner;
+    private EditText _heightText;
+    private EditText _weightText;
+    private Spinner _activeSpinner;
+    private Button _createUser;
+
+    private User _currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout);
+
+        _nameText = findViewById(R.id.name_text);
+        _ageText = findViewById(R.id.age_text);
+        _genderSpinner = findViewById(R.id.gender_spinner);
+        _heightText = findViewById(R.id.height_text);
+        _weightText = findViewById(R.id.weight_text);
+        _activeSpinner = findViewById(R.id.active_spinner);
+        _createUser = findViewById(R.id.create_user_button);
+
+        User.Gender[] genders = User.Gender.values();
+        User.ActivityFactor[] activityFactors = User.ActivityFactor.values();
+
+        ArrayList<String> genderNames = new ArrayList<String>();
+
+        for (User.Gender i : genders) {
+            genderNames.add(i.toString());
+        }
+
+        ArrayList<String> activityFactorNames = new ArrayList<String>();
+
+        for (User.ActivityFactor i : activityFactors) {
+            activityFactorNames.add(i.toString());
+        }
+
+        ArrayAdapter<String> genderSpinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, genderNames);
+        _genderSpinner.setAdapter(genderSpinnerArrayAdapter);
+
+        ArrayAdapter<String> activityFactorSpinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, activityFactorNames);
+        _activeSpinner.setAdapter(activityFactorSpinnerArrayAdapter);
+
+        _createUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = _nameText.getText().toString();
+                int age = Integer.parseInt(_ageText. getText().toString());
+                User.Gender gender = User.Gender.valueOf(_genderSpinner.getSelectedItem().toString());
+                double height = Double.parseDouble(_heightText.getText().toString());
+                double weight = Double.parseDouble(_weightText.getText().toString());
+                User.ActivityFactor activityFactor = User.ActivityFactor.valueOf(_activeSpinner.getSelectedItem().toString());
+
+                _currentUser = new User(name, age, gender, height, weight, activityFactor);
+
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
